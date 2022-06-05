e855da5353d21ddea6783e4f9930cd36f763b15f
Zisis Maras <zisismaras@gmail.com>
2022-06-05 13:34:23 +0300

    change bugfix branch to 2.1.x

--- builds/default.js
+++ builds/default.js
@@ -8,6 +8,6 @@ module.exports = {
         repo: "https://github.com/MarlinFirmware/Configurations/",
         path: "/config/examples/Creality/Ender-3/CrealityV422/",
         stable_branch: "release-{{marlin_version}}",
-        nightly_branch: "bugfix-2.0.x"
+        nightly_branch: "bugfix-2.1.x"
     }
 };
