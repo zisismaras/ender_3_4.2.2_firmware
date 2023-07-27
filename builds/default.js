921d8b01d0e8c9f1129e14fd81358c26f9d7a3ba
Zisis Maras <contact@zisismaras.me>
2023-07-27 23:24:58 +0300

    use min_version 2.1.2

--- builds/default.js
+++ builds/default.js
@@ -1,5 +1,6 @@
 module.exports = {
     board_env: "STM32F103RE_creality",
+    min_version: "2.1.2",
     meta: {
         stable_name: "ender_3_4.2.2-{{marlin_version}}-default-{{uid}}",
         nightly_name: "ender_3_4.2.2-{{current_date}}-default-{{uid}}"
